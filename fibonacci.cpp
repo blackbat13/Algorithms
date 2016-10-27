0e66b8155ca51bbf4f5154db75e3c0c968ecea83
blackbat13 <blackbat13@gmail.com>
2016-10-27 21:10:08 +0200

    Added fibonacci numbers

---
+++ fibonacci.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Damian Kurpiewski on 27.10.2016.
+//
+
+#include <iostream>
+
+using namespace std;
+
+/// Count n'th fibonacci number using iterative method
+int fibonacci_iterative(int n) {
+    if (n <= 2) {
+        return 1;
+    }
+
+    int f1 = 1, f2 = 1;
+    for (int i = 3; i <= n; i++) {
+        int f3 = f1 + f2;
+        f1 = f2;
+        f2 = f3;
+    }
+
+    return f2;
+}
+
+/// Count n'th fibonacci number using recursive method
+int fibonacci_recursive(int n) {
+    if (n <= 2) {
+        return 1;
+    }
+
+    return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
+}
+
+int main() {
+    int n;
+    cout << "Enter number to compute: ";
+    cin >> n;
+
+    cout << "Iterative: " << fibonacci_iterative(n) << endl;
+    cout << "Recursive: " << fibonacci_recursive(n) << endl;
+
+    return 0;
+}
