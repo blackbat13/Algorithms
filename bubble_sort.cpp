02049a588183d7d311893e231bda7887f2f38de0
blackbat13 <blackbat13@gmail.com>
2016-10-27 20:54:36 +0200

    Added merge sort

---
+++ bubble_sort.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Damian Kurpiewski on 27.10.2016.
+//
+
+#include <iostream>
+using namespace std;
+
+/// Sort ascending array of specified length
+void bubble_sort(int array[], int length) {
+    for(int i = 0; i < length; i++) {
+        for(int j = length - 1; j > i; j--) {
+            if(array[j] < array[j-1]) {
+                swap(array[j], array[j-1]);
+            }
+        }
+    }
+}
+
+int main() {
+    int array[10] = {7, 3, 0, 1, 5, 2, 5, 19, 10, 5};
+    bubble_sort(array, 10);
+
+    for(int i = 0; i < 10; ++i) {
+        cout << array[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
