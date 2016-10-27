b22f28d2b589800a3741a86efa00995ece483420
blackbat13 <blackbat13@gmail.com>
2016-10-27 23:00:31 +0200

    Added perfect check

---
+++ integers/perfect.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Damian Kurpiewski on 27.10.2016.
+//
+
+//
+// Created by Damian Kurpiewski on 27.10.2016.
+//
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/// Check if n is perfect
+bool is_perfect(int n) {
+    int sum = 0;
+
+    for(int i = 1; i < n; i++) {
+        if(n % i == 0) {
+            sum += i;
+        }
+    }
+
+    return sum == n;
+}
+
+int main() {
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
+    if(is_perfect(n)) {
+        cout << n << " is perfect" << endl;
+    } else {
+        cout << n << " is not perfect" << endl;
+    }
+
+    return 0;
+}
