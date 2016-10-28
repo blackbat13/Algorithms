12896128a7f70f71f0200091d37ad41108489c97
blackbat13 <blackbat13@gmail.com>
2016-10-28 23:53:29 +0200

    Added computing prime factors

---
+++ integers/prime_factors.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Damian Kurpiewski on 28.10.2016.
+//
+
+#include <iostream>
+using namespace std;
+
+/// Given integer n print its prime factors
+void distribute(int n) {
+    int i = 2;
+    while(n > 1) {
+        if(n % i == 0) {
+            cout << i << " ";
+            n /= i;
+        } else {
+            i++;
+        }
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
+
+    cout << "Prime factors of " << n << ": ";
+    distribute(n);
+
+    return 0;
+}
