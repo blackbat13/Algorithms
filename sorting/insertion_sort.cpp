1f9bb63bd55cf978a003f1e260f110f7ce3c0b9b
blackbat13 <blackbat13@gmail.com>
2017-06-22 23:51:06 +0200

    Simplified insertion sort

--- sorting/insertion_sort.cpp
+++ sorting/insertion_sort.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
+
 using namespace std;
 
 /// Sort ascending array of specified length
 /// \param array - array to sort
 /// \param length - length of given array
 void insertion_sort(int array[], int length) {
-    for(int i = 1; i < length; i++) {
-        for(int j = i; j > 0; j--) {
-            if(array[j] < array[j-1]) {
-                swap(array[j], array[j-1]);
-            } else {
-                break;
-            }
+    for (int i = 1; i < length; i++) {
+        int j = i;
+        while (j > 0 && array[j] < array[j - 1]) {
+            swap(array[j], array[j - 1]);
+            j--;
         }
     }
 }
@@ -20,7 +19,7 @@ int main() {
     int array[10] = {7, 3, 0, 1, 5, 2, 5, 19, 10, 5};
     insertion_sort(array, 10);
 
-    for(int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 10; i++) {
         cout << array[i] << " ";
     }
 
