b96d2c6579167a057d242141d4b91bc267ba06bb
blackbat13 <blackbat13@gmail.com>
2019-07-24 12:20:08 +0200

    Added stack and queue implementation of quicksort

--- sorting/quicksort.cpp
+++ sorting/quicksort.cpp
@@ -3,6 +3,9 @@
 //
 
 #include <iostream>
+#include <stack>
+#include <map>
+#include <queue>
 
 using namespace std;
 
@@ -36,9 +39,89 @@ void quick_sort(int tab[], int left, int right) {
     quick_sort(tab, i, right);
 }
 
+void quick_sort_stack(int tab[], int n) {
+    stack<pair<int, int> > st;
+    st.push(make_pair(0, n - 1));
+    while(!st.empty()) {
+        pair<int, int> el = st.top();
+        st.pop();
+        int left = el.first;
+        int right = el.second;
+
+        if (right <= left) {
+            continue;
+        }
+
+        int pivot = tab[(left + right) / 2];
+        int i = left, j = right;
+        while (i <= j) {
+            while (tab[i] < pivot) {
+                i++;
+            }
+
+            while (pivot < tab[j]) {
+                j--;
+            }
+
+            if (i > j) {
+                break;
+            }
+
+            swap(tab[i], tab[j]);
+
+            i++;
+            j--;
+        }
+
+        st.push(make_pair(left, j));
+        st.push(make_pair(i, right));
+    }
+}
+
+void quick_sort_queue(int tab[], int n) {
+    queue<pair<int, int> > qu;
+    qu.push(make_pair(0, n - 1));
+    while(!qu.empty()) {
+        pair<int, int> el = qu.front();
+        qu.pop();
+        int left = el.first;
+        int right = el.second;
+
+        if (right <= left) {
+            continue;
+        }
+
+        int pivot = tab[(left + right) / 2];
+        int i = left, j = right;
+        while (i <= j) {
+            while (tab[i] < pivot) {
+                i++;
+            }
+
+            while (pivot < tab[j]) {
+                j--;
+            }
+
+            if (i > j) {
+                break;
+            }
+
+            swap(tab[i], tab[j]);
+
+            i++;
+            j--;
+        }
+
+        qu.push(make_pair(left, j));
+        qu.push(make_pair(i, right));
+    }
+}
+
 int main() {
     int tab[] = {7, 2, -2, 7, 7, 293, 1, 5, 94, -5};
-    quick_sort(tab, 0, 9);
+     quick_sort(tab, 0, 9);
+//    quick_sort_stack(tab, 10);
+//    quick_sort_queue(tab, 10);
     for (int i = 0; i < 10; i++) {
         cout << tab[i] << " ";
     }
